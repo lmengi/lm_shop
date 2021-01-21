267c8202e8ecaee6f97b63419748a12f9bb3be19
小木 <jlwswk@126.com>
2021-01-22 01:26:16 +0800

    [master][微服务][NA][NA]商城微服务基本架构。

---
+++ shop-coupon/src/test/java/com/lm/shop/coupon/ShopCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lm.shop.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
